58bd34f591f120a42a3546b73f1b9f7222e960b6
dva <denisva@gmail.com>
2018-08-13 23:17:45 +0300

    linted files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { Component } from "react";
-import { hot } from "react-hot-loader";
-import "./App.css";
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './App.css';
 
-const App = () => <div>Hello World!</div>
+const App = () => (
+  <div>
+    Hello World!
+  </div>
+);
 
 export default hot(module)(App);
