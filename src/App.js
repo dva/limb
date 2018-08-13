5c8b276eab8a991d6b77a123a314f7d379184b71
dva <denisva@gmail.com>
2018-08-13 22:36:48 +0300

    hmr

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 import "./App.css";
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
-}
+const App = () => <div>Hello World!</div>
 
-export default App;
+export default hot(module)(App);
