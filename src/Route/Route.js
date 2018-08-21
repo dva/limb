c652ec98e64ed2dda174a680c4bff2da4c481b80
dva <denisva@gmail.com>
2018-08-22 01:16:23 +0300

    rendering routed components per link

--- src/Route/Route.js
+++ src/Route/Route.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from '../component/Home';
 import About from '../component/About';
+import Header from '../common/Header';
 
 export default () => (
   <Router>
     <React.Fragment>
-      <Home />
-      <About />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </React.Fragment>
   </Router>
 );
