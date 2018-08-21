909e2ec1bdda4c2f12b2512735fa9c5fc43596d2
dva <denisva@gmail.com>
2018-08-22 01:39:33 +0300

    basic type checking

--- src/common/NotFound.js
+++ src/common/NotFound.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default ({ location }) => (
+const NotFound = ({ location }) => (
   <div>
     No match for:&nbsp;
     <code>{location.pathname}</code>
   </div>
 );
+
+NotFound.propTypes = {
+  location: PropTypes.string,
+};
+
+NotFound.defaultProps = {
+  location: '',
+};
+
+export default NotFound;
