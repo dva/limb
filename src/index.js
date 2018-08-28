11f11fa5a1e3c8d5f4aa30155f3c52c57043dd6b
dva <denisva@gmail.com>
2018-08-28 17:46:32 +0300

    debug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
+import debug from 'debug';
 
 import App from './App';
 
-console.log(_.defaults({ foo: 1 }, { foo: 3, bar: 2 }));
+debug.log(_.defaults({ foo: 1 }, { foo: 3, bar: 2 }));
 
 ReactDOM.render(
   <App />,
