2cba44fd7bc0d454642622a3296eeee47ea521a4
dva <denisva@gmail.com>
2018-08-28 18:08:36 +0300

    luxon (improved Moment)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { hot, setConfig } from 'react-hot-loader';
+import { DateTime } from 'luxon';
+import _ from 'lodash';
+import debug from 'debug';
 
 import Route from './Route';
 
 import './styles/App.scss';
 
 setConfig({ logLevel: 'debug' });
+
+debug.log(`%cinfo: ${DateTime.utc().toLocal()}`, 'background: #222; color: #bada55');
+debug.log(_.defaults({ foo: 1 }, { foo: 3, bar: 2 }));
 
 const App = () => (
   <Route />
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import _ from 'lodash';
-import debug from 'debug';
 
 import App from './App';
-
-debug.log(_.defaults({ foo: 1 }, { foo: 3, bar: 2 }));
 
 ReactDOM.render(
   <App />,
