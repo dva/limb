d3fbc91b62cc2e2183e0e62a7f9bdefff013c1c4
dva <denisva@gmail.com>
2019-01-16 01:22:03 +0200

    useEffect hook example

--- src/component/Hooks/Hooks.tsx
+++ src/component/Hooks/Hooks.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Example() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    console.log(`You clicked ${count} times`);
+  }, [count]);
 
   const handleClick = () => {
     setCount(count + 1);
