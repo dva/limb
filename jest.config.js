b4c0551121d9a2ec99e980ad005a510977d6cc2c
dva <denisva@gmail.com>
2019-03-23 19:46:14 +0200

    improving config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
+const path = require('path')
+
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts',
+  rootDir: path.resolve(process.cwd(), 'src'),
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    utils: '<rootDir>/src/utils'
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    utils: '<rootDir>/utils'
   }
 }
