fd6f68848f9f118f0ce0a2d676085b17e5182231
dva <denisva@gmail.com>
2019-04-03 22:13:45 +0300

    fixed alias

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,6 @@ module.exports = {
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
     '^@utils$': '<rootDir>/utils/'
-  }
+  },
+  setupFilesAfterEnv: ['<rootDir>/../enzyme.config.js']
 }
