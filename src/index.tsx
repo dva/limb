986af42df7ca8a42b0dadfbe1e894640a285452c
dva <denisva@gmail.com>
2019-11-04 23:50:35 +0200

    moved SW file to its own

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(registration => {
-        console.log('SW registered: ', registration);
-      })
-      .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      });
-  });
-}
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
